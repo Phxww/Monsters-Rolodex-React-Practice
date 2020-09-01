cb4e1e2c7dec849fdecbef7af5c54809f6469447
Phxww <wwbphx@gmail.com>
2020-09-01 22:33:44 +0800

    3-29  Card List Component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,43 @@
 import React,{Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import {CardList} from './components/card-list/card-list.component';
 
 class App extends Component {
   constructor(){
     super()
 
     this.state = {
-      monsters:[
-        {
-          id:'1',
-          name: 'Zombie1'
-        },{
-          id:'2',
-          name: 'Zombie2'
-        },{
-          id:'3',
-          name: 'Zombie3'
-        }
-      ]
+      monsters:[]
     }
+  }
+
+  componentDidMount(){
+    
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then( response =>  response.json())
+    .then( users =>{
+      console.log("user:",users)
+      console.log("this.setState.monsters:",this.setState.monsters)
+      this.setState({monsters : users})}
+      
+      
+      )
+
   }
 
   render(){
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <CardList name='Phx'>
           {
             this.state.monsters.map(monster=>(
             <h1 key={monster.id}>{monster.name}</h1>
             ))
           }
+        </CardList>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+         
           <a
             className="App-link"
             href="https://reactjs.org"
